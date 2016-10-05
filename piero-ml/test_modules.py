c16cfff2bda19bbdea1f8b14ac08fbdce21ec49d
Teketel <for.teketel@gmail.com>
2016-10-05 11:50:34 +0300

    project splited

---
+++ piero-ml/test_modules.py
@@ -0,0 +1,9 @@
+# Check if  modules are installed
+import sklearn
+import numpy
+import networkx
+import progressbar
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.preprocessing import normalize
+from sklearn.grid_search import GridSearchCV
